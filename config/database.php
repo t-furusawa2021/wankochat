e519c239d8a3ef529c333944bacb4a0c56974b30
t-furusawa2021 <t-furusawa@b-tm.co.jp>
2021-08-08 21:54:00 +0900

    使用するライブラリの追加

--- config/database.php
+++ config/database.php
@@ -127,11 +127,10 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'database' => 0,
         ],
 
         'cache' => [
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import moment from 'moment'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+window.Vue = Vue;
+window.moment = moment
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+const router =  new VueRouter({
+    // モードの設定
+    mode: 'history',
+    routes: [
+        {
+            //
+        }
+    ]
+});
+
+const app = new Vue({
+    el: '#app',
+    router,
+    components: {
+        //
+    }
+});
